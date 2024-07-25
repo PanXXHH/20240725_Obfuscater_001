c214a3c25dd62a0a6b62c940ea1cc330f3d04a64
PanXXHH <987384390@qq.com>
2024-07-25 23:01:51 +0800

    #updated(20240725(001,002,003))

--- __init__.py
+++ __init__.py
@@ -1,21 +1,16 @@
 import fire
 import os
 import sys
-# import time
 
 global global_targetpath
 
-
-# def xor_data(data, key):
-#     """对数据进行异或混淆。"""
-#     return bytes(a ^ b for a, b in zip(data, key * (len(data) // len(key) + 1)))
 
 def bit_flip_bytes(bytes_data):
     # 对字节数组进行位反转
     return bytes(byte ^ 0xFF for byte in bytes_data)
 
 
-def _obfuscate_header(input_file, minimum_size=1024*1024):
+def _obfuscate_header(input_file: str, noback: bool = False, minimum_size: int = 1024*1024):
     # 计算头部大小为文件的1%，但不少于1MB
     file_size = os.path.getsize(input_file)
     encrypt_size = max(int(file_size * 0.01), minimum_size)
@@ -30,24 +25,24 @@ def _obfuscate_header(input_file, minimum_size=1024*1024):
     print(f"正在从 {input_file} 读取数据中...")
     with open(input_file, 'rb+') as file:
         header = file.read(encrypt_size)  # 读取需要混淆的头部数据
-        print(f"正在创建头部的备份...")
-        with open(backup_file, 'wb') as backup:
-            backup.write(header)  # 备份头部数据
+        if noback:
+            print(f"不需要创建头部备份！")
+        else:
+            print(f"正在创建头部的备份...")
+            with open(backup_file, 'wb') as backup:
+                backup.write(header)  # 备份头部数据
 
-        file.seek(0)  # 重置文件指针到开始位置
-        # key = password.encode()  # 使用密码生成密钥
         print(f"正在混淆 {encrypt_size} 字节的数据...")
         obfuscated_header = bit_flip_bytes(header)
+        file.seek(0)  # 重置文件指针到开始位置
         file.write(obfuscated_header)  # 直接写回原文件
 
     output_file = input_file + ".obf001"
     os.rename(input_file, output_file)
     print("混淆完成。如果操作失败，请使用备份文件恢复数据。")
-    # print("正在删除备份文件...")
-    # os.remove(backup_file)
 
 
-def _deobfuscate_header(input_file, minimum_size=1024*1024):
+def _deobfuscate_header(input_file: str, minimum_size: int = 1024*1024):
 
     if not input_file.endswith(".obf001"):
         print("错误: 文件后缀不正确，应以 '.obf001' 结尾")
@@ -69,19 +64,31 @@ def _deobfuscate_header(input_file, minimum_size=1024*1024):
 
     os.rename(input_file, original_file)
     print("解混淆完成。")
+    print(f"正在尝试删除备份文件...")
     backup_file = original_file + ".headerbak"
-    print("正在删除备份文件...")
-    os.remove(backup_file)
-
-
-def default_cmd(input_file: str | None, deobfuscate: bool | None):
-    if not deobfuscate:
-        _obfuscate_header(input_file=input_file)
+    if not os.path.exists(backup_file):
+        print(f"备份文件 {backup_file} 不存在，不需要删除！")
     else:
+        print(f"正在删除备份文件 {backup_file}")
+        os.remove(backup_file)
+
+
+def default_cmd(input_file: str | None, deobfuscate: bool | None, noback: bool | None):
+    input_file = input_file if input_file is not None else input("请输入文件名：")
+    deobfuscate = deobfuscate if deobfuscate is not None else input_file.endswith(
+        ".obf001")
+    noback = noback if noback is not None else False
+
+    if deobfuscate:
         _deobfuscate_header(input_file=input_file)
+    else:
+        _obfuscate_header(input_file=input_file, noback=noback)
 
 
-def obfuscate_folder(deobfuscate: bool = False):
+def obfuscate_folder_cmd(deobfuscate: bool | None, noback: bool | None):
+    deobfuscate = deobfuscate if deobfuscate is not None else False
+    noback = noback if noback is not None else False
+
     for root, dirs, files in os.walk(global_targetpath):  # 遍历文件夹
         for filename in files:
             file_path = os.path.join(root, filename)
@@ -89,7 +96,7 @@ def obfuscate_folder(deobfuscate: bool = False):
                 if file_path.endswith(".obf001"):
                     try:
                         _deobfuscate_header(input_file=file_path)
-                        print(f"文件 {filename} 已解混淆。")
+                        # print(f"文件 {filename} 已解混淆。")
                     except Exception as e:
                         print(f"解混淆文件 {filename} 时发生错误：{e}")
             else:
@@ -102,43 +109,20 @@ def obfuscate_folder(deobfuscate: bool = False):
                     continue
 
                 try:
-                    _obfuscate_header(input_file=file_path)
-                    print(f"文件 {filename} 已混淆。")
+                    _obfuscate_header(input_file=file_path, noback=noback)
+                    # print(f"文件 {filename} 已混淆。")
                 except Exception as e:
                     print(f"混淆文件 {filename} 时发生错误：{e}")
 
 
-def main(command=None, targetpath=None, folder=None, deobfuscate=None, de=None,noback=None):
+def main(command=None, targetpath=None, all=None, deobfuscate=None, de=None, noback=None):
     global global_targetpath
     global_targetpath = targetpath or '.'
-    if folder is True:
-        obfuscate_folder(deobfuscate=deobfuscate or de)
-    elif command is None:
-        filename = input("请输入文件名：")
-        default_cmd(input_file=filename,
-                    deobfuscate=deobfuscate or de or filename.endswith(".obf001"))
-    elif command is not None:
-        default_cmd(input_file=command,
-                    deobfuscate=deobfuscate or de or command.endswith(".obf001"))
+    if all is True:
+        obfuscate_folder_cmd(deobfuscate=deobfuscate or de, noback=noback)
     else:
-        print("无法理解！")
-
-        # def main():
-        #     if len(sys.argv) != 4:
-        #         print("用法: python script.py <模式> <文件> <密码>")
-        #         sys.exit(1)
-
-        #     mode = sys.argv[1]
-        #     file = sys.argv[2]
-        #     password = sys.argv[3]
-
-        #     if mode == 'obfuscate':
-        #         obfuscate_header(file, password)
-        #     elif mode == 'deobfuscate':
-        #         deobfuscate_header(file, password)
-        #     else:
-        #         print("无效模式。使用 'obfuscate' 或 'deobfuscate'.")
-        #         sys.exit(1)
+        default_cmd(input_file=command,
+                    deobfuscate=deobfuscate or de)
 
 
 if __name__ == "__main__":
